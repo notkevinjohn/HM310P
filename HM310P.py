e3c571ebbe43c02877b98493caae9abca2fcbe5e
Protokolmaker <82887145+Protokolmaker@users.noreply.github.com>
2021-04-21 15:42:39 +0200

    Add fuctions to read set valus (V, C, OVP and OCP) and Read C
    New fuctions:
        get_set_voltage, returns the set voltage
        get_current, returns current current output
        get_set_current, returns the set current
        get_set_overvoltageprotection, returns the set overvoltageprotection
        get_set_overcurrentprotection, returns the set overcurrentprotection
    All funktions with Commer have now a extra Space (from (x,y) to (x, y)

--- HM310P.py
+++ HM310P.py
@@ -4,9 +4,11 @@ import minimalmodbus
 import serial
 import time
 
+
 class HM310P():
 
     rDepth = 100
+
     def __init__(self):
         self.supply = minimalmodbus.Instrument('/dev/dcpowersupply', 1, minimalmodbus.MODE_RTU)
         self.supply.serial.baudrate = 9600
@@ -33,7 +35,7 @@ class HM310P():
 
     def read_power(self):
         try:
-            power = self.supply.read_register(1,0)
+            power = self.supply.read_register(1, 0)
             return power
         except:
             return "Error"
@@ -79,7 +81,25 @@ class HM310P():
 
     def read_voltage(self):
         try:
-            voltage = self.supply.read_register(16,2)
+            voltage = self.supply.read_register(16, 2)
+            return voltage
+        except:
+            return "Error"
+
+    def get_set_voltage(self):
+        r = 0
+        value = "Error"
+        while r <= self.rDepth:
+            value = self.read_set_voltage()
+            if not value == "Error":
+                return value
+            r += 1
+            time.sleep(0.001)
+        return False
+
+    def read_set_voltage(self):
+        try:
+            voltage = self.supply.read_register(48, 2)
             return voltage
         except:
             return "Error"
@@ -104,7 +124,44 @@ class HM310P():
     
 ############################
 #### Current Management ####
-############################  
+############################
+
+    def get_current(self):
+        r = 0
+        value = "Error"
+        while r <= self.rDepth:
+            value = self.read_voltage()
+            if not value == "Error":
+                return value
+            r += 1
+            time.sleep(0.001)
+        return False
+
+    def read_current(self):
+        try:
+            voltage = self.supply.read_register(17, 2)
+            return voltage
+        except:
+            return "Error"
+
+    def get_set_current(self):
+        r = 0
+        value = "Error"
+        while r <= self.rDepth:
+            value = self.read_set_current()
+            if not value == "Error":
+                return value
+            r += 1
+            time.sleep(0.001)
+        return False
+
+    def read_set_current(self):
+        try:
+            voltage = self.supply.read_register(49, 2)
+            return voltage
+        except:
+            return "Error"
+
     def set_current(self, current):
         r = 0
         value = "Error"
@@ -126,6 +183,42 @@ class HM310P():
 ###############################
 #### Protection Management ####
 ###############################
+
+    def get_set_overvoltageprotection(self):
+        r = 0
+        value = "Error"
+        while r <= self.rDepth:
+            value = self.read_set_overvoltageprotection()
+            if not value == "Error":
+                return value
+            r += 1
+            time.sleep(0.001)
+        return False
+
+    def read_set_overvoltageprotection(self):
+        try:
+            voltage = self.supply.read_register(32, 2)
+            return voltage
+        except:
+            return "Error"
+
+    def get_set_overcurrentprotection(self):
+        r = 0
+        value = "Error"
+        while r <= self.rDepth:
+            value = self.read_set_overcurrentprotection()
+            if not value == "Error":
+                return value
+            r += 1
+            time.sleep(0.001)
+        return False
+
+    def read_set_overcurrentprotection(self):
+        try:
+            voltage = self.supply.read_register(33, 2)
+            return voltage
+        except:
+            return "Error"
     
     def set_overvoltageprotection(self, voltage):
         r = 0
@@ -163,13 +256,15 @@ class HM310P():
         except:
             return "Error"
 
+
 if __name__ == "__main__":
     supply = HM310P()
     supply.set_voltage(0)
     supply.power_on()
     time.sleep(5)  
-    for i in range(0,101):
+    for i in range(0, 101):
         supply.set_voltage(i/100*5)
         time.sleep(3)
         
     supply.power_off()
+
